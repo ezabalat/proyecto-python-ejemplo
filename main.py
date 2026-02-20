3c5b20d01bbb01ff2738fe628a050d6fa5f405bb
Eduardo Zabala <ezabalat@ucenfotec.ac.cr>
2026-02-20 14:30:32 -0600

    Agrega función despedirse al programa principal

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def saludar(nombre):
     return f"Hola, {nombre}. Bienvenido a GitHub 🚀"
 
+def despedirse(nombre):
+    return f"Hasta luego, {nombre}. Sigue programando 💻"
+
 if __name__ == "__main__":
     nombre = input("Ingrese su nombre: ")
     print(saludar(nombre))
+    print(despedirse(nombre))
